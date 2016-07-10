9a2ad0666c2b980c06d83bb6a44b68d72d204977
JB <rj_burnett@yahoo.com>
2016-07-10 16:15:46 -0400

    adding basic addition

---
+++ work1/basic-addition.r
@@ -0,0 +1,14 @@
+#!/home/jbconda3/anaconda3/bin/Rscript
+#
+# Calculate 3 + 4 and assign to x
+x <- 3 + 4
+
+# Calculate 6 + 12 and assign to y
+y <- 6 + 12
+
+# add x and y together and assign to z
+z <- x + y
+
+# print z
+print(z)
+
